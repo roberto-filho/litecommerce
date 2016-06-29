2fa6288e7d5de351c6214df39155a9ad5bb57c77
Roberto Filho <roberto.w.filho@gmail.com>
2016-06-29 00:54:13 -0300

    A little bit more of code.

---
+++ src/main/java/org/filho/litecommerce/model/Produto.java
@@ -0,0 +1,41 @@
+package org.filho.litecommerce.model;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+import javax.persistence.*;
+
+/**
+ * Entity implementation class for Entity: Produto
+ *
+ */
+@Entity
+public class Produto implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id @GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	private String nome;
+	private BigDecimal price;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public BigDecimal getPrice() {
+		return price;
+	}
+	public void setPrice(BigDecimal price) {
+		this.price = price;
+	}
+	
+}
